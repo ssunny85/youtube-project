9a4213d778a89757500cb66e1521ad687716469e
ssunny <ssssunny85@gmail.com>
2021-05-03 10:44:18 +0900

    youtube service axios 정리

--- src/service/youtube.js
+++ src/service/youtube.js
@@ -2,23 +2,36 @@ import axios from 'axios';
 
 class Youtube {
   constructor(key) {
+    this.youtube = axios.create({
+      baseURL: process.env.REACT_APP_API_URL,
+      params: { key },
+    });
     this.key = key;
   };
 
   async mostPopular() {
     try {
-      const { data } = await axios.get(
-        `${process.env.REACT_APP_API_URL}/videos?part=snippet&chart=mostPopular&maxResults=25&key=${this.key}`
-      );
+      const { data } = await this.youtube.get('videos', {
+        params: {
+          part: 'snippet',
+          chart: 'mostPopular',
+          maxResults: 25,
+        },
+      });
       return data.items;
     } catch (err) {
       console.log('err: ', err);
     }
   };
   async search(keyword) {
-    const { data } = await axios.get(
-      `${process.env.REACT_APP_API_URL}/search?part=snippet&maxResults=25&type=video&q=${keyword}&key=${this.key}`
-    );
+    const { data } = await this.youtube.get('search', {
+      params: {
+        part: 'snippet',
+        maxResults: 25,
+        type: 'video',
+        q: keyword,
+      },
+    });
     data.items = data.items.map(item => ({
       ...item,
       id: item.id.videoId,
